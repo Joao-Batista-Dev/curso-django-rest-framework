28ecee7c9788a1ee3e55b07825c72503a18e5ce4
Joao Batista <joaobldss@gmail.com>
2025-07-16 15:51:25 -0300

    Router, urlpatterns, path e include no urls.py do Django Rest Framework

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,59 +1,28 @@
-from django.urls import path
-
+from django.urls import path, include
 from .views import site
 from .views import api
+from rest_framework.routers import SimpleRouter
+
 
 app_name = 'recipes'
 
+
+recipe_api_v2_router = SimpleRouter()
+recipe_api_v2_router.register(
+    'recipes/api/v2',
+    api.RecipeApiv2ViewSet,
+    basename='recipes-api',
+)
+
 urlpatterns = [
-    path(
-        '',
-        site.RecipeListViewHome.as_view(),
-        name="home"
-    ),
-    path(
-        'recipes/search/',
-        site.RecipeListViewSearch.as_view(),
-        name="search"
-    ),
-    path(
-        'recipes/tags/<slug:slug>/',
-        site.RecipeListViewTag.as_view(),
-        name="tag"
-    ),
-    path(
-        'recipes/category/<int:category_id>/',
-        site.RecipeListViewCategory.as_view(),
-        name="category"
-    ),
-    path(
-        'recipes/<int:pk>/',
-        site.RecipeDetail.as_view(),
-        name="recipe"
-    ),
-    path(
-        'recipes/api/v1/',
-        site.RecipeListViewHomeApi.as_view(),
-        name="recipes_api_v1",
-    ),
-    path(
-        'recipes/api/v1/<int:pk>/',
-        site.RecipeDetailAPI.as_view(),
-        name="recipes_api_v1_detail",
-    ),
-    path(
-        'recipes/theory/',
-        site.theory,
-        name='theory',
-    ),
-    path('recipes/api/v2/', api.RecipeApiv2ViewSet.as_view({
-        'get': 'list',
-        'post': 'create',
-    }), name='recipes_api_v2'),
-    path('recipes/api/v2/<int:pk>/', api.RecipeApiv2ViewSet.as_view({
-        'get': 'retrive',
-        'patch': 'partial_update',
-        'delete': 'destroy',
-    }), name='recipes_api_v2_detail',),
+    path('', site.RecipeListViewHome.as_view(), name="home"),
+    path('recipes/search/', site.RecipeListViewSearch.as_view(), name="search"),
+    path('recipes/tags/<slug:slug>/', site.RecipeListViewTag.as_view(), name="tag"),
+    path('recipes/category/<int:category_id>/', site.RecipeListViewCategory.as_view(), name="category"),
+    path('recipes/<int:pk>/', site.RecipeDetail.as_view(),name="recipe"),
+    path('recipes/api/v1/', site.RecipeListViewHomeApi.as_view(), name="recipes_api_v1",),
+    path('recipes/api/v1/<int:pk>/', site.RecipeDetailAPI.as_view(), name="recipes_api_v1_detail",),
+    path('recipes/theory/', site.theory, name='theory',),
     path('recipes/api/v2/tag/<int:pk>/', api.tag_api_detail, name='recipes_api_v2_tag',),
+    path('', include(recipe_api_v2_router.urls)),
 ]
